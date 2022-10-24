738d2898dc84b9d26176b6c1c825f8f7b6bfdb1d
JackyGLZ <jakilita@gmail.com>
2022-10-24 05:09:22 -0500

    MICROSERVICIO SMS Y CORREO ELECTRONICO

--- MS-Notificaciones/notificacion.py
+++ MS-Notificaciones/notificacion.py
@@ -2,14 +2,18 @@
 import os
 from flask import Flask,redirect,url_for,render_template,request
 from twilio.rest import Client
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 app=Flask(__name__)
 
+#Enviroment Twilio
 @app.route('/')
 def inicio():
     twilio_sid = os.environ.get('TWILIO_ACCOUNT_SID')
     return 'SID =', twilio_sid
 
+#Servicio de envío de mensajes SMS con Twilio
 
 @app.route('/sms')
 def sms():
@@ -32,5 +36,27 @@ def sms():
     return 'Mensaje enviado de manera exitosa'
 
 
+#Servicio de envío de correo electrónico con Sengrid
+
+@app.route('/email')
+def email():
+    message = Mail(
+        from_email= request.args.get ('correo'),
+        to_emails= request.args.get ('destinatario'),
+        subject= request.args.get ('asunto'),
+        html_content='<h1>envio correos equipo Energy Grupo 39</h1>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message) 
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+        return ('El correo fue enviado de manera exitosa')
+    except Exception as e:
+        print(e.message)
+        return 'el correo no se pudo enviar'
+    
+
 if __name__== '__main__':
     app.run()
+    
